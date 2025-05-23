0c64b4253d39a6baa9c43d26f5b1dc6f5e3123f8
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-05-23 14:58:54 +0530

    Update main.c

--- bms_project_/main.c
+++ bms_project_/main.c
@@ -2,14 +2,22 @@
 #include "adc.h"
 #include "uart.h"
 #include <util/delay.h>
+#include "current.h"
+#include "temperature.h"
 
 int main(void) {
     adc_init();
     uart_init();
     bms_init();
+    current_init();
+    temperature_init();
 
     while (1) {
         bms_update();
+        check_current_protection();
+        check_temperature_protection();
         _delay_ms(1000);
     }
 }
+
+

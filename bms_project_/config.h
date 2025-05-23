71391f0f3801da30d3181be6c2afb37d1958d31c
Rimjhim Agrawal <101130751+rimjhim-ag@users.noreply.github.com>
2025-05-23 14:58:26 +0530

    Update config.h

--- bms_project_/config.h
+++ bms_project_/config.h
@@ -8,5 +8,10 @@
 #define MIN_CELL_VOLTAGE 3.0
 #define MAX_CELL_VOLTAGE 4.2
 #define BALANCE_THRESHOLD 0.05
+#define MAX_CURRENT 10.0   // Amps
+#define MIN_CURRENT 0.1    // Amps
+#define MAX_TEMPERATURE 60.0 // Celsius
+#define MIN_TEMPERATURE 0.0  // Celsius
+
 
 #endif
